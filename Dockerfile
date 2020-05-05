6f3366b1af997ba2ce7b66c55ca56d1387647fd9
Reinaldo de Souza <rsou@hellofresh.com>
2020-05-05 10:46:32 +0200

    Make the image compatible with Astronomer's helm chart

--- Dockerfile
+++ Dockerfile
@@ -30,6 +30,7 @@ RUN set -ex && \
     add-apt-repository -y ppa:deadsnakes/ppa && \
     apt-get install -y build-essential python3.7 python3.7-dev python3-pip && \
     apt-get install -y libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev python3-setuptools && \
+    apt-get install -y netcat && \
     update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1 && \
     update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1 && \
     update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1 && \
@@ -42,12 +43,16 @@ RUN set -ex && \
       pyasn1 \
       psycopg2-binary \
       requests-oauthlib==${REQUESTS_OAUTHLIB_VERSION} \
-      apache-airflow[postgres,ssh,s3,statsd]==${AIRFLOW_VERSION} \
+      apache-airflow[postgres,ssh,s3,statsd,kubernetes]==${AIRFLOW_VERSION} \
       prometheus-client &&\
     pip3 install -U dynaconf[yaml,vault] && \
     # pin version since it has breaking changes
-    pip3 install -U Werkzeug==${WERKZEUG_VERSION}
+    pip3 install -U Werkzeug==${WERKZEUG_VERSION} && \
+    pip3 install --no-cache-dir "https://github.com/astronomer/astronomer-airflow-scripts/releases/download/v0.0.4/astronomer_airflow_scripts-0.0.4-py3-none-any.whl"
 
+COPY ./dags ${AIRFLOW_USER_HOME}/dags
+COPY ./plugins ${AIRFLOW_USER_HOME}/plugins
+COPY ./clean-airflow-logs /usr/local/bin/clean-airflow-logs
 COPY entrypoint.sh /entrypoint.sh
 
 RUN chown -R airflow: ${AIRFLOW_USER_HOME}
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,42 @@
-#!/usr/bin/env sh
-AIRFLOW__CORE__EXECUTOR=${EXECUTOR}
-WEBSERVER_PORT=${WEBSERVER_PORT}
+#!/usr/bin/env bash
+set -e
 
-case "$2" in
-  webserver)
-    airflow initdb
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
-      airflow scheduler &
-      airflow webserver
-    fi
-    ;;
-  *)
-    echo "INFO: parameter passed: $2"
-    exec "$@"
-    ;;
-esac
+# Compatibility with older helm charts that used to run crond.
+if [[ "$1" == "crond" ]]; then
+  exec /usr/local/bin/clean-airflow-logs
+fi
+
+# Airflow subcommand
+CMD=$2
+
+url_parse_regex="[^:]+://([^@/]*@)?([^/:]*):?([0-9]*)/?"
+
+# Wait for postgres then init the db
+if [[ -n $AIRFLOW__CORE__SQL_ALCHEMY_CONN  ]]; then
+  # Wait for database port to open up
+  [[ ${AIRFLOW__CORE__SQL_ALCHEMY_CONN} =~ $url_parse_regex ]]
+  HOST=${BASH_REMATCH[2]}
+  PORT=${BASH_REMATCH[3]}
+  echo "Waiting for host: ${HOST} ${PORT}"
+  while ! nc -w 1 -z "${HOST}" "${PORT}"; do
+    sleep 0.001
+  done
+fi
+
+if [[ -n $AIRFLOW__CELERY__BROKER_URL ]] && [[ $CMD =~ ^(scheduler|worker|flower)$ ]]; then
+  # Wait for database port to open up
+  [[ ${AIRFLOW__CELERY__BROKER_URL} =~ $url_parse_regex ]]
+  HOST=${BASH_REMATCH[2]}
+  PORT=${BASH_REMATCH[3]}
+  echo "Waiting for host: ${HOST} ${PORT}"
+  while ! nc -w 1 -z "${HOST}" "${PORT}"; do
+    sleep 0.001
+  done
+fi
+
+if [[ $CMD == "webserver" ]]; then
+  airflow sync_perm
+fi
+
+# Run the original command
+exec "$@"
